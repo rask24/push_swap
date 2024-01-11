4316c2aaec0d819d726bee05980018435c520328
rask24 <rask0x18@gmail.com>
2024-01-11 18:30:41 +0900

    refactor test_is_invalid_argument

--- test/test_is_invalid_argument.cpp
+++ test/test_is_invalid_argument.cpp
@@ -6,50 +6,66 @@ extern "C" {
   #include "push_swap.h"
 }
 
-TEST(is_invalid_argument, one_argument_valid) {
+TEST(is_invalid_argument, OneArgumentValid) {
   int argc = 2;
   const char *args[] = { "push_swap", "1" };
   char **argv = const_cast<char **>(args);
 
-  EXPECT_EQ(is_invalid_argument(argc, argv), false);
+  EXPECT_FALSE(is_invalid_argument(argc, argv));
 }
 
-TEST(is_invalid_argument, one_argument_not_digit) {
+TEST(is_invalid_argument, ArgumentIsZero) {
+  int argc = 2;
+  const char *args[] = { "push_swap", "0" };
+  char **argv = const_cast<char **>(args);
+
+  EXPECT_FALSE(is_invalid_argument(argc, argv));
+}
+
+TEST(is_invalid_argument, ArgumentIsNegative) {
+  int argc = 2;
+  const char *args[] = { "push_swap", "-42" };
+  char **argv = const_cast<char **>(args);
+
+  EXPECT_FALSE(is_invalid_argument(argc, argv));
+}
+
+TEST(is_invalid_argument, OneArgumentNonDigit) {
   int argc = 2;
   const char *args[] = { "push_swap", "abc" };
   char **argv = const_cast<char **>(args);
 
-  EXPECT_EQ(is_invalid_argument(argc, argv), true);
+  EXPECT_TRUE(is_invalid_argument(argc, argv));
 }
 
-TEST(is_invalid_argument, one_argument_including_not_digit) {
+TEST(is_invalid_argument, OneArgumentIncludingNonDigit) {
   int argc = 2;
   const char *args[] = { "push_swap", "42abc" };
   char **argv = const_cast<char **>(args);
 
-  EXPECT_EQ(is_invalid_argument(argc, argv), true);
+  EXPECT_TRUE(is_invalid_argument(argc, argv));
 }
 
-TEST(is_invalid_argument, one_argument_max_overflow) {
+TEST(is_invalid_argument, OneArgumentMaxOverflow) {
   int argc = 2;
-  const char *args[] = { "push_swap", "2147483648" };
+  const char *args[] = { "push_swap", "2147483648" };  // INT_MAX + 1
   char **argv = const_cast<char **>(args);
 
-  EXPECT_EQ(is_invalid_argument(argc, argv), true);
+  EXPECT_TRUE(is_invalid_argument(argc, argv));
 }
 
-TEST(is_invalid_argument, one_argument_min_overflow) {
+TEST(is_invalid_argument, OneArgumentMinOverflow) {
   int argc = 2;
-  const char *args[] = { "push_swap", "-2147483649" };
+  const char *args[] = { "push_swap", "-2147483649" };  // INT_MIN - 1
   char **argv = const_cast<char **>(args);
 
-  EXPECT_EQ(is_invalid_argument(argc, argv), true);
+  EXPECT_TRUE(is_invalid_argument(argc, argv));
 }
 
-TEST(is_invalid_argument, multiple_argument_has_duplicate) {
+TEST(is_invalid_argument, MultipleArgumentsWithDuplicate) {
   int argc = 5;
-  const char *args[] = { "push_swap", "1", "3", "5", "+1" };
+  const char *args[] = { "push_swap", "1", "3", "5", "1" };  // Duplicate '1'
   char **argv = const_cast<char **>(args);
 
-  EXPECT_EQ(is_invalid_argument(argc, argv), true);
+  EXPECT_TRUE(is_invalid_argument(argc, argv));
 }
