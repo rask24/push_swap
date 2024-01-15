ae0a2d3897f82b2ef580590b72151acb3a0dbbdf
rask24 <rask0x18@gmail.com>
2024-01-15 13:08:09 +0900

    convert is_invalid_argument to check_args

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 12:37:54 by reasuke           #+#    #+#             */
-/*   Updated: 2024/01/12 16:02:39 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/01/15 13:07:40 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@
 
 # include "../libft/libft.h"
 
-bool	is_invalid_argument(int argc, char **argv);
+void	check_args(int argc, char **argv);
 
 t_list	*generate_stack(int argc, char **argv);
 
