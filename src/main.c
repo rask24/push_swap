da3e994d95754f7540bde5f48c8174526644eaf6
rask24 <rask0x18@gmail.com>
2024-01-16 13:14:34 +0900

    modify sort argument

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 12:37:36 by reasuke           #+#    #+#             */
-/*   Updated: 2024/01/15 16:59:58 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/01/16 13:13:30 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@ int	main(int argc, char **argv)
 	check_args(argc, argv);
 	stack_a = generate_stack(argc, argv);
 	stack_b = NULL;
-	sort(&stack_a, &stack_b, argc);
+	sort(&stack_a, &stack_b, argc - 1);
 	ft_lstclear(&stack_a, free);
 	return (0);
 }
--- src/sort/sort.c
+++ src/sort/sort.c
@@ -6,17 +6,14 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 15:10:35 by reasuke           #+#    #+#             */
-/*   Updated: 2024/01/15 16:31:27 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/01/16 13:13:45 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	sort(t_list **stack_a, t_list **stack_b, int argc)
+void	sort(t_list **stack_a, t_list **stack_b, int num_of_element)
 {
-	int	num_of_element;
-
-	num_of_element = argc - 1;
 	if (num_of_element <= 3)
 		nano_sort(stack_a, num_of_element);
 	else if (num_of_element == 4)
