c6ac73ef651db61f8d3b891c84f6ad5150440094
rask24 <rask0x18@gmail.com>
2024-02-14 18:04:08 +0900

    init rspec

--- test/e2e/check.rb
+++ test/e2e/check.rb
@@ -3,11 +3,11 @@
 a = [1, 2, 3, 4, 5]
 
 a.permutation.to_a.each do |array|
-  str = +'./push_swap'
+  str = +"./push_swap"
   array.each do |item|
     str << " #{item}"
   end
-  str << ' | wc -l | bc'
+  str << " | wc -l | bc"
   puts str
   system str, exception: true
   puts "\n"
