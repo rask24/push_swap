1b3bd3e16644959a15c4dd2bdde8061be2ceca28
rask24 <70057885+rask24@users.noreply.github.com>
2024-02-14 20:48:49 +0900

    check allowed functions (#38)
    * add shell script
    
    * refactor test
    
    * update ci
    
    * check
    
    * check2
    
    * build on macos
    
    * fix ci
    
    * fix shell script

---
+++ test/check_allowed_functions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+check_binary_symbols() {
+    local binary="$1"
+    if [ ! -f $binary ]; then
+        echo "Error: Binary file '$binary' not found."
+        exit 1
+    fi
+    local undefined_symbols=$(nm -u "$binary" | sed 's/^_*//')
+    for symbol in $undefined_symbols; do
+        if [[ ! " ${allowed_functions[@]} " =~ " ${symbol}" ]]; then
+            echo "Error: $binary: Disallowed function used: ${symbol}"
+            exit 1
+        fi
+    done
+}
+
+allowed_functions=("read" "write" "malloc" "free" "exit" "error" "dyld_stub_binder")
+
+# Check symbols for push_swap
+check_binary_symbols "push_swap"
+
+# Check symbols for checker
+check_binary_symbols "checker"
+
+echo "All used functions are allowed."
