6148583d758619f0fbcc9796e329a6ad22fe4864
rask24 <rask0x18@gmail.com>
2024-02-14 20:27:31 +0900

    refactor test

--- test/check_allowed_functions.sh
+++ test/check_allowed_functions.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
+check_binary_symbols() {
+    local binary="$1"
+    if [ ! -f $binary ]; then
+        echo "Error: Binary file '$binary' not found."
+        exit 1
+    fi
+    local undefined_symbols=$(nm -u "$binary" | tr -d '_')
+    for symbol in $undefined_symbols; do
+        if [[ ! " ${allowed_functions[@]} " =~ " ${symbol}" ]]; then
+            echo "Error: $binary: Disallowed function used: ${symbol}"
+            exit 1
+        fi
+    done
+}
+
 allowed_functions=("read" "write" "malloc" "free" "exit" "error")
 
-# push_swap
-undefined_symbols=$(nm -u push_swap | tr -d '_')
-for symbol in $undefined_symbols; do
-  if [[ ! " ${allowed_functions[@]} " =~ " ${symbol} " ]]; then
-    echo "Error: push_swap: Disallowed function used: ${symbol}"
-    exit 1
-  fi
-done
+# Check symbols for push_swap
+check_binary_symbols "push_swap"
 
-# checker
-undefined_symbols=$(nm -u checker | tr -d '_')
-for symbol in $undefined_symbols; do
-  if [[ ! " ${allowed_functions[@]} " =~ " ${symbol} " ]]; then
-    echo "Error: checker: Disallowed function used: ${symbol}"
-    exit 1
-  fi
-done
+# Check symbols for checker
+check_binary_symbols "checker"
 
 echo "All used functions are allowed."
