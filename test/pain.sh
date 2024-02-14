4585c0555b24f767811a557b50aced571343812f
rask24 <rask0x18@gmail.com>
2024-02-14 21:05:28 +0900

    fix pain.sh

--- test/pain.sh
+++ test/pain.sh
@@ -9,8 +9,8 @@ NBR_LINK=$( echo "$NBR" | tr ' ' ',' )
 CMD_LINK=$( echo "$CMD" | sed -e"s/rra/g/g" -e"s/rrb/h/g" \
 -e"s/rrr/i/g" -e"s/sa/a/g" -e"s/sb/b/g" -e"s/ss/c/g" \
 -e"s/ra/d/g" -e"s/rb/e/g" -e"s/rr/f/g"  -e"s/pa/j/g" -e"s/pb/k/g" | tr -d '\n')
-# CMD_COUNT=${#CMD_LINK}
-RESULT=$( echo $CMD | ./checker $NBR )
+CMD_COUNT=${#CMD_LINK}
+# RESULT=$( echo $CMD | ./checker $NBR )
 echo "Operations: $CMD_COUNT"
 echo "Result $RESULT"
 open "https://kaaaaakun.github.io/push-swap-pain/?&nbr=$NBR_LINK&cmd=$CMD_LINK"
