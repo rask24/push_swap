ffa10974d68a1af029df53203f28e9ff7a389fde
rask24 <rask0x18@gmail.com>
2024-02-18 16:59:07 +0900

    update error case spec

--- test/e2e/spec/error_cases_spec.rb
+++ test/e2e/spec/error_cases_spec.rb
@@ -3,21 +3,39 @@
 require_relative "../helper/command_helper"
 
 describe "Error cases for push_swap" do
-  it "returns error for non integer inputs" do
-    _stdout, stderr, status = execute_push_swap("a", "b", "c")
+  describe "when inputs are not integers" do
+    it "reports an error if all arguments are non-integer" do
+      _stdout, stderr, status = execute_push_swap("a", "b", "c")
 
-    expect(stderr).to(eq("Error\n"))
-    expect(status.exitstatus).to(eq(1))
+      expect(stderr).to(eq("Error\n"))
+      expect(status.exitstatus).to(eq(1))
+    end
+
+    it "reports an error if any argument is non-integer" do
+      _stdout, stderr, status = execute_push_swap("3", "push_swap", "5", "4", "1", "0", "2")
+
+      expect(stderr).to(eq("Error\n"))
+      expect(status.exitstatus).to(eq(1))
+    end
   end
 
-  it "returns error for duplicates" do
-    _stdout, stderr, status = execute_push_swap("3", "2", "5", "4", "1", "0", "2")
+  describe "when there are duplicate inputs" do
+    it "reports an error if all arguments are the same" do
+      _stdout, stderr, status = execute_push_swap("3", "3", "3", "3", "3", "3", "3")
 
-    expect(stderr).to(eq("Error\n"))
-    expect(status.exitstatus).to(eq(1))
+      expect(stderr).to(eq("Error\n"))
+      expect(status.exitstatus).to(eq(1))
+    end
+
+    it "reports an error if there are any duplicate arguments" do
+      _stdout, stderr, status = execute_push_swap("3", "2", "5", "4", "1", "0", "2")
+
+      expect(stderr).to(eq("Error\n"))
+      expect(status.exitstatus).to(eq(1))
+    end
   end
 
-  it "returns error for overflow" do
+  it "reports an error for integer overflow" do
     _stdout, stderr, status = execute_push_swap("0", "3", "2", "34578347984", "1")
 
     expect(stderr).to(eq("Error\n"))
