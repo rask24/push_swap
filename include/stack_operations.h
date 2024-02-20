9c44fc30f3fa21747367640494c8e92d285b7736
rask24 <rask0x18@gmail.com>
2024-02-20 14:17:17 +0900

    fix order stack_operations.h

--- include/stack_operations.h
+++ include/stack_operations.h
@@ -6,7 +6,7 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/11 12:41:01 by reasuke           #+#    #+#             */
-/*   Updated: 2024/02/11 12:41:49 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/02/20 14:16:47 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,17 @@
 
 # include "push_swap.h"
 
-void	operate_sa(t_stack **p_a);
+void	push_stack(t_stack **p_s1, t_stack **p_s2);
+void	swap_stack(t_stack **p_stack);
+void	rotate_stack(t_stack **p_stack);
+void	reverse_rotate_stack(t_stack **p_stack);
 
+void	do_single_n_operations(t_stack **p_st, int n,
+			void (*operation)(t_stack **));
+void	do_double_n_operations(t_stack **p_a, t_stack **p_b, int n,
+			void (*operation)(t_stack **, t_stack **));
+
+void	operate_sa(t_stack **p_a);
 void	operate_sb(t_stack **p_b);
 void	operate_ss(t_stack **p_a, t_stack **p_b);
 void	operate_ra(t_stack **p_a);
@@ -27,13 +36,5 @@ void	operate_rrb(t_stack **p_a);
 void	operate_rrr(t_stack **p_a, t_stack **p_b);
 void	operate_pa(t_stack **p_b, t_stack **p_a);
 void	operate_pb(t_stack **p_a, t_stack **p_b);
-void	do_single_n_operations(t_stack **p_st, int n,
-			void (*operation)(t_stack **));
-void	do_double_n_operations(t_stack **p_a, t_stack **p_b, int n,
-			void (*operation)(t_stack **, t_stack **));
 
-void	push_stack(t_stack **p_s1, t_stack **p_s2);
-void	swap_stack(t_stack **p_stack);
-void	rotate_stack(t_stack **p_stack);
-void	reverse_rotate_stack(t_stack **p_stack);
 #endif
