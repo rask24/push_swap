6e5af0c6ba439b5ff75d5d0d8a528c4f186181e8
rask24 <rask0x18@gmail.com>
2024-04-20 15:41:25 +0900

    define t_operation

--- src/stack_operations/repeat_dual_stack_operations.c
+++ src/stack_operations/repeat_dual_stack_operations.c
@@ -6,15 +6,15 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 19:22:50 by reasuke           #+#    #+#             */
-/*   Updated: 2024/04/20 15:25:42 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/04/20 15:40:46 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "stack_operations.h"
 
 void	repeat_dual_stack_operations(t_stack **p_a, t_stack **p_b, int n,
-		void (*operation)(t_stack **, t_stack **))
+		t_dual_operation *op)
 {
 	while (n--)
-		operation(p_a, p_b);
+		op(p_a, p_b);
 }
--- src/stack_operations/repeat_stack_operations.c
+++ src/stack_operations/repeat_stack_operations.c
@@ -6,15 +6,14 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 19:22:26 by reasuke           #+#    #+#             */
-/*   Updated: 2024/04/20 15:32:16 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/04/20 15:39:53 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "stack_operations.h"
 
-void	repeat_stack_operations(t_stack **p_st, int n,
-		void (*operation)(t_stack **))
+void	repeat_stack_operations(t_stack **p_st, int n, t_operation *op)
 {
 	while (n--)
-		operation(p_st);
+		op(p_st);
 }
