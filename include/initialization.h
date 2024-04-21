4564111196c5c80a00a03cfd7bdf1cf3271dafd1
rask24 <rask0x18@gmail.com>
2024-04-21 16:26:42 +0900

    update libft

--- include/initialization.h
+++ include/initialization.h
@@ -6,13 +6,14 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/11 12:30:37 by reasuke           #+#    #+#             */
-/*   Updated: 2024/02/11 12:33:50 by reasuke          ###   ########.fr       */
+/*   Updated: 2024/04/21 16:25:21 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef INITIALIZATION_H
 # define INITIALIZATION_H
 
+# include <errno.h>
 # include "push_swap.h"
 
 int		check_args(int argc, char **argv);
